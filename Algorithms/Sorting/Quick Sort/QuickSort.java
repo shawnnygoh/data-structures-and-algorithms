cc0a3c01bd55ae9cd99d88fb4ac9bc4e4a6534eb
Shawn <shawnnygoh@gmail.com>
2024-06-20 01:47:34 +0000

    feat: add Quick Sort to Sorrting Algorithms

---
+++ Algorithms/Sorting/Quick Sort/QuickSort.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class QuickSort {
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        int i;
+        int size = arr.length;
+
+        for (i = 0; i < size; i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array: ");
+        int arr_size = 0;
+        if (sc.hasNextInt()) {
+            arr_size = sc.nextInt();
+        }
+
+        int[] arr = new int[arr_size];
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < arr_size; i++) {
+            if (sc.hasNextInt()) {
+                arr[i] = sc.nextInt();
+            }
+        }
+
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("Sorted Array: ");
+        printArray(arr);
+        sc.close();
+    }
+}
